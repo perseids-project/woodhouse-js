3030e37efca50456dbc1a4964c810084cf8607c9
zfletch <zfletch2@gmail.com>
2018-11-19 15:44:31 -0500

    use perseids header and footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { PerseidsHeader, PerseidsFooter } from 'perseids-react-components';
+
+import 'perseids-react-components/build/css/index.css';
 
 import AsyncLookup from './AsyncLookup';
-import Footer from './Footer';
 
 const App = () => (
   <React.Fragment>
+    <PerseidsHeader>
+      Woodhouse English-Greek Dictionary
+    </PerseidsHeader>
     <div className="container text-center">
       <header>
         <h1 className="h3 pt-4 mb-1 font-weight-normal">
-          Woodhouse English-Greek Dictionary (beta)
+          English-Greek Dictionary
         </h1>
+        <h4 className="h5 mb-2 font-weight-normal">
+          <em>
+            A Vocabulary of the Attic Language
+          </em>
+        </h4>
         <h5 className="h5 mb-3 font-weight-normal">
-          Type a word below
+          S. C. Woodhouse
         </h5>
       </header>
       <main>
@@ -23,9 +33,10 @@ const App = () => (
         </div>
       </main>
     </div>
-    <footer>
-      <Footer />
-    </footer>
+    <PerseidsFooter
+      github="https://github.com/perseids-project/woodhouse-js"
+      report="https://github.com/perseids-project/woodhouse-js/issues"
+    />
   </React.Fragment>
 );
 
