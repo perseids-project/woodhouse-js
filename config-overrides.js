0909fb0bab4260c4d938933b8dc1e04e449a9b3a
zfletch <zfletch2@gmail.com>
2019-01-14 12:22:51 -0500

    fix lint

--- config-overrides.js
+++ config-overrides.js
@@ -16,6 +16,7 @@ module.exports = {
     // This is very fragile and liable to change with updates to React Scripts
     // It is necessary in order to keep build sizes down since there is a lot of Unicode
     // See https://github.com/facebook/create-react-app/issues/2706
+    // eslint-disable-next-line no-param-reassign
     config.optimization.minimizer[0].options.terserOptions.output.ascii_only = false;
 
     return config;
