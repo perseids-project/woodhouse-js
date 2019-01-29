522162b41f45890237b34d7d2c3a9f4761510ee8
zfletch <zfletch2@gmail.com>
2019-01-29 09:33:29 -0500

    search functionality
    allow users to search and browse

--- src/lib/Parser.js
+++ src/lib/Parser.js
@@ -1,6 +1,24 @@
+const findClosest = (inOrder, string, min = 0, max = inOrder.length - 1) => {
+  const middle = Math.round((min + max) / 2);
+  const value = inOrder[middle];
+
+  if (min >= max) {
+    return max;
+  } if (max === min + 1) {
+    if (string > inOrder[min]) {
+      return max;
+    }
+    return min;
+  } if (string < value) {
+    return findClosest(inOrder, string, min, middle);
+  }
+  return findClosest(inOrder, string, middle, max);
+};
+
 class Parser {
   constructor(dictionary) {
     this.matchers = [dictionary.exact, dictionary.greek, dictionary.latin];
+    this.inOrder = dictionary.inOrder;
     this.dictionary = dictionary.dictionary;
   }
 
@@ -23,6 +41,23 @@ class Parser {
     });
 
     return results;
+  }
+
+  wordsAt(string, count = 26) {
+    const key = string.toLowerCase().normalize();
+    const { inOrder } = this;
+    const closest = findClosest(inOrder, key);
+    const entries = [];
+
+    for (let ii = closest; ii < Math.min(closest + count, inOrder.length); ii += 1) {
+      entries.push({ index: ii, entry: inOrder[ii] });
+    }
+
+    return {
+      entries,
+      previous: inOrder[Math.max(0, closest - 1)],
+      next: inOrder[Math.min(inOrder.length - 1, closest + 1)],
+    };
   }
 }
 
