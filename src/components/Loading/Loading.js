6971f89acb5805671fa4e5ca351fc02aa8ec1a8d
Benjamin <benjamin.s.auerbach@gmail.com>
2019-02-08 14:52:59 -0500

    use css modules

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Loading.css';
+import './Loading.module.css';
 
 const Loading = ({ text }) => (
   <div className="mt-2">
